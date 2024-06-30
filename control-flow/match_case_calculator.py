23b282b26e76acce209b6a38c5621f86dc62c433
Atmani Reda <ATMANI.REDA.AR@GMAIL.COM>
2024-06-30 13:44:27 +0100

    Update match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,7 +1,7 @@
 num1=int(input("Enter the first number:"))
 num2=int(input("Enter the second number:"))
-Op=input("Choose the operation (+, -, *, /):")
-match Op:
+operation=input("Choose the operation (+, -, *, /):")
+match operation:
     case "+":
         R=num1+num2
         print("the result is",R)
